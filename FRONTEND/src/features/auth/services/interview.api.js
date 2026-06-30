95424c4c6f5311ee28c47dfc907f439332f215fc
Utsav Panchal <utsavpanchal1304@gmail.com>
2026-06-30 17:20:16 +0530

    Added interview.api.js

---
+++ FRONTEND/src/features/auth/services/interview.api.js
@@ -0,0 +1,52 @@
+import axios from 'axios'
+
+const api = axios.create({
+    baseURL: 'http://localhost:3000' , 
+    withCredentials: true 
+})
+
+export const generateInterviewReport = async ({ jobDescription , selfDescription , resumeFile }) => {
+
+    const formData  = new FormData()
+    formData.append('jobDescription' , jobDescription);
+    formData.append('selfDescription' , selfDescription);
+    formData.append('resume' , resumeFile);
+
+
+    const response = await api.post('/api/interview' , formData, {
+
+        headers: {
+            'Content-Type':'multipart/form-data'
+        }
+    })
+
+    return response.data
+
+
+}
+
+export const getInterviewReportById = async (interviewId) => {
+
+    //const response = await api.get('/api/interview/report/${interviewId}' )
+    const response = await api.get(`/api/interview/report/${interviewId}`)
+    return response.data 
+
+}
+
+export const getAllInterviewReports = async () => {
+
+    const response = await api.get('/api/interview')
+
+    return response.data 
+
+}
+
+export const getResume =
+  async (interviewId) => {
+    const response =
+      await api.get(
+        `/api/interview/resume/${interviewId}`
+      );
+
+    return response.data;
+  };
